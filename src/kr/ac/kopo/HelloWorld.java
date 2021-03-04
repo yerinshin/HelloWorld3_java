09dafd12f0277169d6b37100fb3979f833299e62
yerinshin <dpfls0106@gmail.com>
2021-03-04 11:39:25 +0900

    first commit

---
+++ src/kr/ac/kopo/HelloWorld.java
@@ -0,0 +1,14 @@
+package kr.ac.kopo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		System.out.println("Hi, there!");
+		
+
+	}
+
+}
